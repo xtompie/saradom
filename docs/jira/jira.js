c8df0d1483d7db00e8f280ffb1ba7e490a92b7d5
Tomasz Pielczyk <tomasz.pielczyk@gmail.com>
2026-09-13 20:22:25 +0200

    Jira demo: send fragment requests as GET so static hosting serves them

--- docs/jira/jira.js
+++ docs/jira/jira.js
@@ -360,15 +360,16 @@ App.Ui.Hx = (() => {
     const orig = window.fetch;
     let seq = 7;
     window.fetch = async (url, opts) => {
-        const res = await orig(url, opts);
         if (!opts || !opts.body) {
-            return res;
+            return orig(url, opts);
         }
+        const body = opts.body;
+        const res = await orig(url, { method: 'GET' });
         const text = await res.text();
         if (!text.includes('{{')) {
             return new Response(text, { status: res.status, statusText: res.statusText, headers: res.headers });
         }
-        const params = opts.body instanceof FormData ? opts.body : new URLSearchParams(opts.body);
+        const params = body instanceof FormData ? body : new URLSearchParams(body);
         let out = text;
         for (const [k, v] of params.entries()) {
             out = out.replaceAll('{{' + k + '}}', v);
